65d3d39407a7b0c081db2375b011a39a9e836aa6
Michał Walczak <michal.walczak@blockydevs.com>
2024-02-08 17:17:59 +0100

    e2e test wip

--- packages/apps/human-app/server/test/app.e2e-spec.ts
+++ packages/apps/human-app/server/test/app.e2e-spec.ts
@@ -2,16 +2,16 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import request from 'supertest';
 import { AppModule } from '../src/app.module';
-import nock from "nock";
-import { ConfigModule, ConfigService } from "@nestjs/config";
-import { WorkerType } from "../src/interfaces/signup-worker-request.dto";
-import {describe} from '@jest/globals';
+import { ConfigModule } from '@nestjs/config';
+import { describe } from '@jest/globals';
+import { generateWorkerSignupRequestBody } from './fixtures/user-worker.fixture';
 
-describe('AppController (e2e)', () => {
+describe('Human APP (e2e) tests', () => {
   let app: INestApplication;
-  let configService: ConfigService;
+  let requestBodyForWorkerSignup: any;
 
-  beforeEach(async () => {
+  before(async () => {
+    requestBodyForWorkerSignup = generateWorkerSignupRequestBody();
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [
         AppModule,
@@ -23,38 +23,21 @@ describe('AppController (e2e)', () => {
     }).compile();
 
     app = moduleFixture.createNestApplication();
-    configService = moduleFixture.get<ConfigService>(ConfigService);
     await app.init();
   });
 
-  it('/ (GET)', () => {
+  it('should successfully process the signup request for a worker', async () => {
     return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+      .post('/auth/signup')
+      .send(requestBodyForWorkerSignup)
+      .expect(201);
   });
 
-  it('should process the singnup worker request', async () => {
-    const registeredType =  WorkerType.WORKER.toString();
-      nock(configService.get<string>('REPUTATION_ORACLE_URL') || 'http://localhost:5678')
-        .post('/auth/signup', {
-          email: 'john_doe@example.com',
-          password: 'v3ry57r0n9P455w0r[)!',
-          type: registeredType,
-        }, {
-          reqheaders: {
-            'Content-Type': 'application/json',
-          },
-        })
-        .reply(200, );
-
-      return request(app.getHttpServer())
-        .post('/auth/signup')
-        .send({
-          email: 'john_doe@example.com',
-          password: 'v3ry57r0n9P455w0r[)!'
-        })
-        .expect(200);
+  it('should return a 409 Conflict status when processing a duplicate signup request for a worker', async () => {
+    return request(app.getHttpServer())
+      .post('/auth/signup')
+      .send(requestBodyForWorkerSignup)
+      .expect(409);
   });
 
   afterAll(async () => {
