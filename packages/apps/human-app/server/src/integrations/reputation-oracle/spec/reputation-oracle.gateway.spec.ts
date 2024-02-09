a4a6d6526a65f7bf837607c7588115320aceb9c9
maciek.nabialek <maciej.nabialek@blockydevs.com>
2024-02-09 12:36:04 +0100

    Chore: test fixes

--- packages/apps/human-app/server/src/integrations/reputation-oracle/spec/reputation-oracle.gateway.spec.ts
+++ packages/apps/human-app/server/src/integrations/reputation-oracle/spec/reputation-oracle.gateway.spec.ts
@@ -8,6 +8,7 @@ import nock from 'nock';
 import { HttpException, HttpStatus } from '@nestjs/common';
 import { SignupOperatorCommand } from '../../../modules/user-operator/interfaces/operator-registration.interface';
 import { gatewayConfigServiceMock } from '../../../common/config/gateway-config.service.mock';
+import { ethers } from 'ethers';
 
 describe('ReputationOracleGateway', () => {
   let service: ReputationOracleGateway;
@@ -102,11 +103,15 @@ describe('ReputationOracleGateway', () => {
   });
 
   describe('sendOperatorSignup', () => {
-    it('should successfully call the reputation oracle operator signup endpoint', async () => {
-      const command = new SignupOperatorCommand(
-        '0x2348237487df12f123a455234',
-        '0x23u4dfa32423daf2314',
+    let exampleCommand: SignupOperatorCommand;
+    beforeEach(async () => {
+      const wallet = ethers.Wallet.createRandom();
+      exampleCommand = new SignupOperatorCommand(
+        wallet.address,
+        await wallet.signMessage('signup'),
       );
+    });
+    it('should successfully call the reputation oracle operator signup endpoint', async () => {
       const expectedData = {
         email: 'asfdsafdd@asdf.cvd',
         password: 'asdfasdf2133!!dasfA',
@@ -117,7 +122,9 @@ describe('ReputationOracleGateway', () => {
         .post('/auth/web3/signup', expectedData)
         .reply(201, '');
 
-      await expect(service.sendOperatorSignup(command)).resolves.not.toThrow();
+      await expect(
+        service.sendOperatorSignup(exampleCommand),
+      ).resolves.not.toThrow();
       expect(httpService.request).toHaveBeenCalled();
     });
   });
--- packages/apps/human-app/server/test/fixtures/user-operator.fixture.ts
+++ packages/apps/human-app/server/test/fixtures/user-operator.fixture.ts
@@ -3,20 +3,10 @@ import { ethers } from 'ethers';
 import { SignupOperatorData } from '../../src/modules/user-operator/interfaces/operator-registration.interface';
 export async function generateOperatorSignupRequestBody() {
   const wallet = ethers.Wallet.createRandom();
-  const flatSig = await wallet.signMessage(generateRandomString());
+  const flatSig = await wallet.signMessage('signup');
   return {
     address: wallet.address,
     signature: flatSig,
     type: UserType.OPERATOR.toString(),
   } as SignupOperatorData;
-}
-function generateRandomString(): string {
-  const characters =
-    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  let result = '';
-  const length = Math.floor(Math.random() * 100);
-  for (let i = 0; i < length; i++) {
-    result += characters.charAt(Math.floor(Math.random() * characters.length));
-  }
-  return result;
 }
