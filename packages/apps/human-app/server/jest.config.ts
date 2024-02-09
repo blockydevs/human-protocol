e7fbb17e358e6258522e43854b714f846de21541
Michał Walczak <michal.walczak@blockydevs.com>
2024-02-09 12:17:47 +0100

    fix: jest config reverted

--- packages/apps/human-app/server/jest.config.ts
+++ packages/apps/human-app/server/jest.config.ts
@@ -1,26 +1,15 @@
 module.exports = {
-  preset: 'ts-jest/presets/default-esm',
-  globals: {
-    'ts-jest': {
-      useESM: true,
-    },
-  },
-  moduleNameMapper: {
-    '^(\\.{1,2}/.*)\\.js$': '$1',
-    '^uuid$': require.resolve('uuid'),
-    '^typeorm$': require.resolve('typeorm'),
-  },
-  extensionsToTreatAsEsm: ['.ts'],
-  transform: {
-    '^.+\\.tsx?$': 'ts-jest',
-  },
   coverageDirectory: '../coverage',
   collectCoverageFrom: ['**/*.(t|j)s'],
   moduleFileExtensions: ['js', 'json', 'ts'],
   rootDir: 'src',
   testEnvironment: 'node',
   testRegex: '.*\\.spec\\.ts$',
-  transformIgnorePatterns: [
-    'node_modules/(?!@nestjs/config/node_modules/uuid)',
-  ],
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  moduleNameMapper: {
+    '^uuid$': require.resolve('uuid'),
+    '^typeorm$': require.resolve('typeorm'),
+  },
 };
