37d7659b158bde354517aa809994c677775657d2
maciek.nabialek <maciej.nabialek@blockydevs.com>
2024-02-09 11:57:38 +0100

    Chore: adds e2e testing for operator registration (sunny day scenario failing), fixes bug wiht modules, adds ethers dependency to the project, removes unnecessary comments

--- packages/apps/human-app/server/jest.config.ts
+++ packages/apps/human-app/server/jest.config.ts
@@ -1,19 +1,17 @@
 module.exports = {
-  preset: 'ts-jest/presets/default-esm', // Use this preset for ESM support
+  preset: 'ts-jest/presets/default-esm',
   globals: {
     'ts-jest': {
       useESM: true,
     },
   },
   moduleNameMapper: {
-    '^(\\.{1,2}/.*)\\.js$': '$1', // Add this line if you're importing ESM modules using .js extensions in your TypeScript files
+    '^(\\.{1,2}/.*)\\.js$': '$1',
     '^uuid$': require.resolve('uuid'),
     '^typeorm$': require.resolve('typeorm'),
   },
-  // Specify file extensions for modules using ESM
   extensionsToTreatAsEsm: ['.ts'],
   transform: {
-    // Use ts-jest for ts / tsx files
     '^.+\\.tsx?$': 'ts-jest',
   },
   coverageDirectory: '../coverage',
@@ -23,6 +21,6 @@ module.exports = {
   testEnvironment: 'node',
   testRegex: '.*\\.spec\\.ts$',
   transformIgnorePatterns: [
-    'node_modules/(?!@nestjs/config/node_modules/uuid)'
+    'node_modules/(?!@nestjs/config/node_modules/uuid)',
   ],
 };
--- packages/apps/human-app/server/test/app.e2e-spec.ts
+++ packages/apps/human-app/server/test/app.e2e-spec.ts
@@ -4,13 +4,13 @@ import request from 'supertest';
 import { AppModule } from '../src/app.module';
 import { generateWorkerSignupRequestBody } from './fixtures/user-worker.fixture';
 import { SignupWorkerData } from '../src/modules/user-worker/interfaces/worker-registration.interface';
+import { beforeAll } from '@jest/globals';
+import { generateOperatorSignupRequestBody } from './fixtures/user-operator.fixture';
+import { SignupOperatorData } from '../src/modules/user-operator/interfaces/operator-registration.interface';
 
 describe('Human APP (e2e) tests', () => {
   let app: INestApplication;
-  let requestBodyForWorkerSignup: SignupWorkerData;
-
   beforeAll(async () => {
-    requestBodyForWorkerSignup = generateWorkerSignupRequestBody();
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
@@ -19,18 +19,45 @@ describe('Human APP (e2e) tests', () => {
     await app.init();
   });
 
-  it('should successfully process the signup request for a worker', async () => {
-    return request(app.getHttpServer())
-      .post('/auth/signup')
-      .send(requestBodyForWorkerSignup)
-      .expect(201);
-  });
+  describe('Worker signup', () => {
+    let requestBodyForWorkerSignup: SignupWorkerData;
+    beforeAll(async () => {
+      requestBodyForWorkerSignup = generateWorkerSignupRequestBody();
+    });
 
-  it('should return a 409 Conflict status when processing a duplicate signup request for a worker', async () => {
-    return request(app.getHttpServer())
-      .post('/auth/signup')
-      .send(requestBodyForWorkerSignup)
-      .expect(409);
+    it('should successfully process the signup request for a worker', async () => {
+      return request(app.getHttpServer())
+        .post('/auth/signup')
+        .send(requestBodyForWorkerSignup)
+        .expect(201);
+    });
+
+    it('should return a 409 Conflict status when processing a duplicate signup request for a worker', async () => {
+      return request(app.getHttpServer())
+        .post('/auth/signup')
+        .send(requestBodyForWorkerSignup)
+        .expect(409);
+    });
+  });
+  describe('Operator signup', () => {
+    let requestBodyForOperatorSignup: SignupOperatorData;
+    beforeAll(async () => {
+      requestBodyForOperatorSignup = await generateOperatorSignupRequestBody();
+      console.log('requestBodyForOperatorSignup', requestBodyForOperatorSignup);
+    });
+    it('should successfully process the signup request for an operator', async () => {
+      return request(app.getHttpServer())
+        .post('/auth/web3/signup')
+        .send(requestBodyForOperatorSignup)
+        .expect(201);
+    });
+
+    it('should return a 409 Conflict status when processing a duplicate signup request for an operator', async () => {
+      return request(app.getHttpServer())
+        .post('/auth/web3/signup')
+        .send(requestBodyForOperatorSignup)
+        .expect(409);
+    });
   });
 
   afterAll(async () => {
