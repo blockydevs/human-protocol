faad87904aa5b24bd288e373645ec3d819c2698b
maciek.nabialek <maciej.nabialek@blockydevs.com>
2024-04-23 14:17:08 +0200

    Review fixes

--- packages/apps/human-app/server/src/common/interfaces/custom-exceptions.interface.ts
+++ packages/apps/human-app/server/src/common/interfaces/custom-exceptions.interface.ts
@@ -1,7 +1,6 @@
 export class EnvironmentVariableMissingError extends Error {
   constructor(variable: string) {
     super(`Environment variable ${variable} is not defined.`);
-    // This line is needed for correct inheritance when targeting ES5
     Object.setPrototypeOf(this, EnvironmentVariableMissingError.prototype);
   }
 }
