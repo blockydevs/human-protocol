dc826c0bb6fb4acb996120f4944ad50612c009a9
MWBlocky <michal.walczak@blockydevs.com>
2024-05-08 16:25:13 +0200

    Remove manual environment variable check.

--- packages/apps/human-app/server/src/common/exceptions/env-variable-missing-error.ts
+++
@@ -1,6 +0,0 @@
-export class EnvVariableMissingError extends Error {
-  constructor(variable: string) {
-    super(`Environment variable(s): {${variable}} not defined.`);
-    Object.setPrototypeOf(this, EnvVariableMissingError.prototype);
-  }
-}
