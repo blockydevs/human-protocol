1274ff35d1bb1c7f8e2628ee7aa57c8b510140f5
MWBlocky <michal.walczak@blockydevs.com>
2024-05-09 17:04:30 +0200

    Update EscrowUtilsGateway tests.

--- packages/apps/human-app/server/src/integrations/escrow/spec/escrow-utils-gateway.spec.ts
+++ packages/apps/human-app/server/src/integrations/escrow/spec/escrow-utils-gateway.spec.ts
@@ -1,6 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { EscrowUtilsGateway } from '../escrow-utils-gateway.service';
 import { EscrowUtils, ChainId } from '@human-protocol/sdk';
+import { NotFoundException } from '@nestjs/common';
 
 jest.mock('@human-protocol/sdk', () => {
   return {
@@ -36,12 +37,11 @@ describe('EscrowUtilsGateway', () => {
 
   describe('getExchangeOracleAddressByEscrowAddress', () => {
     it('should fetch data from EscrowUtils', async () => {
-      const chainId = 1;
       const escrowAddress = 'escrowAddress';
       const expectedUrl = '0x';
 
       const result = await service.getExchangeOracleAddressByEscrowAddress(
-        chainId,
+        ChainId.POLYGON_AMOY,
         escrowAddress,
       );
       expect(EscrowUtils.getEscrow).toHaveBeenCalledWith(
@@ -49,6 +49,19 @@ describe('EscrowUtilsGateway', () => {
         escrowAddress,
       );
       expect(result).toBe(expectedUrl);
+    });
+
+    it('should throw error if Exchange Oracle not found', async () => {
+      const escrowAddress = 'escrowAddress';
+
+      (EscrowUtils.getEscrow as jest.Mock).mockResolvedValue({});
+
+      await expect(
+        service.getExchangeOracleAddressByEscrowAddress(
+          ChainId.POLYGON_AMOY,
+          escrowAddress,
+        ),
+      ).rejects.toThrow(new NotFoundException('Exchange Oracle not found'));
     });
   });
 });
