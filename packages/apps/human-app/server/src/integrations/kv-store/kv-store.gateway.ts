043f189e5c08e5cd826ae2bbeebd0d7a1e466bad
maciej.nabialek <maciej.nabialek@blockydevs.com>
2024-07-01 14:08:33 +0200

    HAM-6

--- packages/apps/human-app/server/src/integrations/kv-store/kv-store.gateway.ts
+++ packages/apps/human-app/server/src/integrations/kv-store/kv-store.gateway.ts
@@ -1,4 +1,4 @@
-import { Inject, Injectable, OnModuleInit } from '@nestjs/common';
+import { HttpException, Inject, Injectable, OnModuleInit } from '@nestjs/common';
 import { EnvironmentConfigService } from '../../common/config/environment-config.service';
 import { ethers } from 'ethers';
 import { KVStoreClient, KVStoreKeys } from '@human-protocol/sdk';
@@ -17,17 +17,24 @@ export class KvStoreGateway {
       new ethers.JsonRpcProvider(this.configService.rpcUrl),
     );
   }
-  async getExchangeOracleUrlByAddress(address: string): Promise<string> {
+  async getExchangeOracleUrlByAddress(address: string): Promise<string | void> {
     const cachedUrl: string | undefined = await this.cacheManager.get(address);
     if (cachedUrl) {
       return cachedUrl;
     }
-    const fetchedUrl = await this.kvStoreClient.get(address, KVStoreKeys.url);
-    await this.cacheManager.set(
-      address,
-      fetchedUrl,
-      this.configService.cacheTtlExchangeOracleUrl,
-    );
-    return fetchedUrl;
+    try {
+      const fetchedUrl = await this.kvStoreClient.get(address, KVStoreKeys.url);
+      await this.cacheManager.set(
+        address,
+        fetchedUrl,
+        this.configService.cacheTtlExchangeOracleUrl,
+      );
+      return fetchedUrl;
+    } catch (e) {
+      if(e.toString().includes('Error: Invalid address'))
+        throw new HttpException(`Unable to retrieve URL from address: ${address}`, 400);
+      else
+        throw new Error(`Error, while fetching exchange oracle URL from kv-store: ${e}`);
+    }
   }
 }
