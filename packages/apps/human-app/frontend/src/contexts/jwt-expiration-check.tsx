084c736d43f59a5e9371b3dca059a3a961a61f87
KacperKoza343 <kacper.koza@blockydevs.com>
2024-07-03 13:33:26 +0200

    feat(app/auth): logout user when tokens expire

---
+++ packages/apps/human-app/frontend/src/contexts/jwt-expiration-check.tsx
@@ -0,0 +1,34 @@
+import type React from 'react';
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+import { useGetAccessTokenMutation } from '@/api/servieces/common/get-access-token';
+import { useWeb3Auth } from '@/auth-web3/use-web3-auth';
+import { useAuth } from '@/auth/use-auth';
+
+export function JWTExpirationCheck({
+  children,
+}: {
+  children: React.ReactElement;
+}) {
+  const web3Auth = useWeb3Auth();
+  const web2Auth = useAuth();
+  const location = useLocation();
+  const { mutate: getAccessTokenMutation } = useGetAccessTokenMutation();
+
+  useEffect(() => {
+    if (web3Auth.user?.exp && web3Auth.user.exp < Date.now() / 1000) {
+      getAccessTokenMutation('web3');
+    }
+
+    if (web2Auth.user?.exp && web2Auth.user.exp < Date.now() / 1000) {
+      getAccessTokenMutation('web2');
+    }
+  }, [
+    location,
+    web3Auth.user?.exp,
+    web2Auth.user?.exp,
+    getAccessTokenMutation,
+  ]);
+
+  return children;
+}
