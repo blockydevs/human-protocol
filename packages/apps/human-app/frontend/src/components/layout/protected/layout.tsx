ff2b9be81a3c99dd8d115814cf89fdd76c82f57a
KacperKoza343 <kacper.koza@blockydevs.com>
2024-07-16 18:26:01 +0200

    feat(app): fixes after review

--- packages/apps/human-app/frontend/src/components/layout/protected/layout.tsx
+++ packages/apps/human-app/frontend/src/components/layout/protected/layout.tsx
@@ -1,6 +1,6 @@
 import { Grid } from '@mui/material';
 import type { Dispatch, SetStateAction } from 'react';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { styled } from '@mui/material/styles';
 import { Outlet } from 'react-router-dom';
 import { useIsMobile } from '@/hooks/use-is-mobile';
@@ -49,6 +49,10 @@ export function Layout({
   ) => JSX.Element;
   renderHCaptchaStatisticsDrawer?: (isOpen: boolean) => JSX.Element;
 }) {
+  const [notificationWith, setNotificationWith] = useState<
+    number | undefined
+  >();
+  const layoutElementRef = useRef<HTMLDivElement>();
   const isHCaptchaLabelingPage = useIsHCaptchaLabelingPage();
   const [notification, setNotification] =
     useState<TopNotificationPayload | null>(null);
@@ -71,6 +75,21 @@ export function Layout({
       setDrawerOpen(true);
     }
   }, [isMobile]);
+
+  useEffect(() => {
+    const setNotificationWidth = () => {
+      if (layoutElementRef.current) {
+        setNotificationWith(layoutElementRef.current.clientWidth);
+      }
+    };
+    setNotificationWidth();
+    window.addEventListener('resize', () => {
+      setNotificationWidth();
+    });
+    return () => {
+      window.removeEventListener('resize', setNotificationWidth);
+    };
+  }, []);
 
   return (
     <ProtectedLayoutContext.Provider
@@ -113,6 +132,7 @@ export function Layout({
           : null}
         <Main isMobile={isMobile} open={drawerOpen}>
           <Grid
+            component="div"
             container
             sx={{
               margin: '1rem 0',
@@ -128,30 +148,59 @@ export function Layout({
             }}
           >
             <Grid
-              item
               sx={{
-                minHeight: '3.2rem',
+                height: '3.2rem',
                 [breakpoints.mobile]: {
-                  minHeight: 'unset',
+                  position: 'absolute',
+                  zIndex: 2000,
+                  top: '0',
+                  left: '0',
+                  width: notificationWith ? `${notificationWith}px` : 'unset',
                 },
               }}
             >
-              {notification ? (
-                <TopNotification
-                  onClose={() => {
-                    setNotification(null);
-                  }}
-                  type={notification.type}
-                >
-                  {notification.content}
-                </TopNotification>
-              ) : null}
+              <Grid
+                item
+                sx={{
+                  minHeight: '3.2rem',
+                  position: 'fixed',
+                  width: notificationWith ? `${notificationWith}px` : 'unset',
+                  zIndex: '10',
+                  [breakpoints.mobile]: {
+                    minHeight: 'unset',
+                  },
+                }}
+              >
+                {notification ? (
+                  <Grid
+                    sx={{
+                      minHeight: '3.2rem',
+                      position: 'relative',
+                      zIndex: '10',
+                    }}
+                  >
+                    <TopNotification
+                      onClose={() => {
+                        setNotification(null);
+                      }}
+                      type={notification.type}
+                    >
+                      {notification.content}
+                    </TopNotification>
+                  </Grid>
+                ) : null}
+              </Grid>
             </Grid>
 
             <Grid item>
               <PageHeader {...pageHeaderProps} />
             </Grid>
-            <Grid sx={{ height: '100%' }}>
+            <Grid
+              component="div"
+              // @ts-expect-error -- ...
+              ref={layoutElementRef}
+              sx={{ height: '100%' }}
+            >
               <Outlet />
             </Grid>
           </Grid>
