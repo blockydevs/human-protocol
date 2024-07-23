b78a120c02699d8c7d458e81e9b912470dcb8a46
KacperKoza343 <kacper.koza@blockydevs.com>
2024-07-23 14:30:51 +0200

    update(app/home): change HMT price api

--- packages/apps/dashboard/ui-2024/src/pages/Home/HMTPrice.tsx
+++ packages/apps/dashboard/ui-2024/src/pages/Home/HMTPrice.tsx
@@ -1,15 +1,17 @@
 import { useHMTPrice } from '../../services/api/use-hmt-price';
 import Typography from '@mui/material/Typography';
+import { handleErrorMessage } from '../../services/handle-error-messsage';
 
 export function HMTPrice() {
-	const { data, status } = useHMTPrice();
+	const { data, status, error } = useHMTPrice();
+	console.log(handleErrorMessage(error));
 	return (
 		<div>
 			<Typography variant="h6" component="p">
 				HMT Price
 			</Typography>
 			<div className="count">
-				{status === 'success' && data['human-protocol'].usd}
+				{status === 'success' && data.hmtPrice}
 				{status === 'pending' && '...'}
 				{status === 'error' && 'No data'}
 			</div>
--- packages/apps/dashboard/ui-2024/src/services/api/use-hmt-price.tsx
+++ packages/apps/dashboard/ui-2024/src/services/api/use-hmt-price.tsx
@@ -1,24 +1,22 @@
 import { useQuery } from '@tanstack/react-query';
-import axios from 'axios';
 import { z } from 'zod';
+import { httpService } from '../http-service';
 import { apiPaths } from '../api-paths';
 
 const successHMTPriceResponseSchema = z.object({
-	'human-protocol': z.object({
-		usd: z.number(),
-	}),
+  hmtPrice: z.number(),
 });
 
 export type HMTPrice = z.infer<typeof successHMTPriceResponseSchema>;
 
 export function useHMTPrice() {
-	return useQuery({
-		queryFn: async () => {
-			const { data } = await axios.get(apiPaths.hmtPrice);
-			const validData = successHMTPriceResponseSchema.parse(data);
+  return useQuery({
+    queryFn: async () => {
+      const { data } = await httpService.get(apiPaths.statsHmtPrice.path);
+      const validData = successHMTPriceResponseSchema.parse(data);
 
-			return validData;
-		},
-		queryKey: ['useHMTPrice'],
-	});
+      return validData;
+    },
+    queryKey: ['useHMTPrice'],
+  });
 }
