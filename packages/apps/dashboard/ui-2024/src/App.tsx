74af26f6d7111b6a67845ce0d03d2b4d1ccd4409
KacperKoza343 <kacper.koza@blockydevs.com>
2024-07-23 15:51:17 +0200

    update(app/home): remove duplicated query client

--- packages/apps/dashboard/ui-2024/src/App.tsx
+++ packages/apps/dashboard/ui-2024/src/App.tsx
@@ -3,27 +3,17 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from '@pages/Home';
 import Graph from '@pages/Graph';
 import SearchResults from '@pages/SearchResults';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-
-const queryClient = new QueryClient({
-	defaultOptions: {
-		mutations: { retry: 0 },
-		queries: { retry: 0 },
-	},
-});
 
 const App: React.FC = () => {
 	return (
-		<QueryClientProvider client={queryClient}>
-			<Router>
-				<Routes>
-					<Route path="/" element={<Home />} />
-					<Route path="/graph" element={<Graph />} />
-					<Route path="/search/:tokenId" element={<SearchResults />} />
-					<Route path="*" element={<div>Not find</div>} />
-				</Routes>
-			</Router>
-		</QueryClientProvider>
+		<Router>
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/graph" element={<Graph />} />
+				<Route path="/search/:tokenId" element={<SearchResults />} />
+				<Route path="*" element={<div>Not find</div>} />
+			</Routes>
+		</Router>
 	);
 };
 
