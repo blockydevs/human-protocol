6daeaed15ac24ef24d155811ebb0832317fccc9c
KacperKoza343 <165884432+KacperKoza343@users.noreply.github.com>
2024-08-22 11:15:12 +0200

    feat(app/leaderboard-page): add page (#221)

--- packages/apps/dashboard/ui-2024/src/components/Home/Leaderboard/Leaderboard.tsx
+++ packages/apps/dashboard/ui-2024/src/components/Home/Leaderboard/Leaderboard.tsx
@@ -4,35 +4,56 @@ import SimpleBar from 'simplebar-react';
 import { Table } from '@components/Home/Leaderboard/components/Table/Table';
 import { SelectNetwork } from '@components/Home/Leaderboard/components/SelectNetwork';
 import { useLeaderboardDetails } from '@services/api/use-leaderboard-details';
-import Loader from '@components/Loader';
-import Stack from '@mui/material/Stack';
-import { handleErrorMessage } from '@services/handle-error-message';
+import Box from '@mui/material/Box';
+import { useNavigate } from 'react-router-dom';
+import { colorPalette } from '@assets/styles/color-palette';
 
 const Leaderboard = () => {
 	const { data, status, error } = useLeaderboardDetails();
+	const navigate = useNavigate();
 
-	if (status === 'pending') {
-		return <Loader height="30vh" />;
-	}
-
-	if (status === 'error') {
-		return (
-			<Stack sx={{ paddingTop: '2rem' }}>{handleErrorMessage(error)}</Stack>
-		);
-	}
+	const isMoreThatFiveEntries = data?.length && data.length > 5;
 
 	return (
 		<>
 			<TableContainer
 				component={Paper}
-				sx={{ padding: '32px', marginTop: '30px' }}
+				sx={{
+					padding: '32px',
+					marginTop: '30px',
+					display: 'flex',
+					flexDirection: 'column',
+					gap: '12px',
+				}}
 			>
 				<div className="mobile-select">
 					<SelectNetwork />
 				</div>
 				<SimpleBar>
-					<Table data={data} />
+					<Table
+						data={isMoreThatFiveEntries ? data.slice(0, 5) : data}
+						status={status}
+						error={error}
+					/>
 				</SimpleBar>
+				{isMoreThatFiveEntries ? (
+					<Box
+						sx={{
+							height: '42px',
+							border: `1px ${colorPalette.primary.main} solid`,
+							borderRadius: '4px',
+							display: 'flex',
+							justifyContent: 'center',
+							alignItems: 'center',
+							cursor: 'pointer',
+						}}
+						onClick={() => {
+							navigate('/leaderboard');
+						}}
+					>
+						View All
+					</Box>
+				) : null}
 			</TableContainer>
 		</>
 	);
